1f13f395f0ef2e826104b48c5be5efbf30aedc99
DocHebi <charlesbascombe54@gmail.com>
2015-02-18 19:52:56 -0500

    Create EntityRobot.java

---
+++ src/main/java/FluxedForces/robotics/EntityRobot.java
@@ -0,0 +1,19 @@
+package fluxedforces.robotics;
+
+import net.minecraft.entity.Entity;
+
+public class EntityRobot {
+	
+	public boolean canSpawn(){
+		
+		/*
+		 * This is suppose to handle if the block next to the Solidering Table has an airblock with the height of 2 blocks, then if true returns true if not then false
+		 */
+		boolean holder = false;
+		return holder;
+	}
+	
+	
+	
+
+}
