deda8c7811ca0305c99343e3cafee643ed67d3ee
DocHebi <charlesbascombe54@gmail.com>
2015-02-18 19:53:16 -0500

    Create Robot.java

---
+++ src/main/java/FluxedForces/robotics/Robot.java
@@ -0,0 +1,41 @@
+package fluxedforces.robotics;
+
+import com.mojang.authlib.GameProfile;
+
+import net.minecraft.world.World;
+
+public class Robot
+{
+	public static void Head()
+	{
+		
+	}
+	
+	
+	public static void Torso()
+	{
+		
+	}
+	
+	public static void Arms()
+	{
+		
+	}
+	
+	public static void Legs()
+	{
+		
+	}
+	
+	public static GameProfile getOwner(World world, GameProfile Name)
+	{
+		
+		/* This suppose to store the player name or UUID to give ownership to the robot.
+		 * 
+		 * 
+		 */
+		
+		return Name;
+		
+	}
+}
