29553830a965fcbdc0209aaf95bc8e5879a0364b
DocHebi <charlesbascombe54@gmail.com>
2015-02-18 19:48:33 -0500

    Create Reference.java

---
+++ src/main/java/FluxedForces/help/Reference.java
@@ -0,0 +1,8 @@
+package fluxedforces.help;
+
+public class Reference 
+{
+	public static final String MODID = "fluxedforces";
+	public static final String NAME = "FluxedForces";
+	public static final String VERSION = "a0.0.1";
+}
