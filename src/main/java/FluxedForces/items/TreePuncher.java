7efb1d564f1e4de95156520b4e4345c50699e957
DocHebi <charlesbascombe54@gmail.com>
2015-02-18 19:51:12 -0500

    Create TreePuncher.java

---
+++ src/main/java/FluxedForces/items/TreePuncher.java
@@ -0,0 +1,18 @@
+package fluxedforces.items;
+
+import fluxedforces.tabs.FluxedForcesTab;
+import fluxedforces.tabs.ModTabs;
+import net.minecraft.item.Item;
+
+
+public class TreePuncher extends Item 
+{
+		public TreePuncher()
+		{
+				super();
+				setMaxStackSize(16);
+				setCreativeTab(ModTabs.tabFluxedForces);
+		}
+	
+	
+}
