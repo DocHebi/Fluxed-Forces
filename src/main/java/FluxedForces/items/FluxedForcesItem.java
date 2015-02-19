c6499cf437200924f6d829f5644f479704c6075e
DocHebi <charlesbascombe54@gmail.com>
2015-02-18 19:50:49 -0500

    Create FluxedForcesItem.java

---
+++ src/main/java/FluxedForces/items/FluxedForcesItem.java
@@ -0,0 +1,16 @@
+package fluxedforces.items;
+
+import fluxedforces.tabs.FluxedForcesTab;
+import fluxedforces.tabs.ModTabs;
+import net.minecraft.item.Item;
+
+public class FluxedForcesItem extends Item {
+	
+	public FluxedForcesItem()
+	{
+		
+		super();
+		setCreativeTab(ModTabs.tabFluxedForces);
+	}
+
+}
